6f0b1430cdc14220e778b510883296e9dff79a53
scpaes <scp2@cin.ufpe.br>
2023-10-02 22:09:41 -0300

    start project

---
+++ dsc-project-one/src/main/java/com/mycompany/dsc/project/one/DscProjectOne.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dsc.project.one;
+
+/**
+ *
+ * @author scpaes
+ */
+public class DscProjectOne {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
