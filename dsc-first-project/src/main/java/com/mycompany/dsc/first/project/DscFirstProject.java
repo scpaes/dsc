e8d1438f93861d86d232ae37342f69fa8941a82c
scpaes <scp2@cin.ufpe.br>
2023-10-06 20:17:44 -0300

    update project

---
+++ dsc-first-project/src/main/java/com/mycompany/dsc/first/project/DscFirstProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dsc.first.project;
+
+/**
+ *
+ * @author scpaes
+ */
+public class DscFirstProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
